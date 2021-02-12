7bfdd374869f9842fdb2764c5ab23d349395eb2c
Miller <1marcusmiller1@gmail.com>
2021-02-12 14:21:21 +0300

    Learn Constant.

--- src/com/learn/Main.java
+++ src/com/learn/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите дробное число: ");
-        double drob1 = scan.nextDouble();
-        double drob2 = scan.nextDouble();
-        double drob3 = scan.nextDouble();
-        System.out.println("Ваши дроби: " + drob1 + " " + drob2 + " " + drob3);
+        final double PI = 3.1415;
+        double p;
+        System.out.print("Введите значение Радиуса круга:\nR = ");
+        int r = scan.nextInt();
+        p = 2 * PI * r;
+        System.out.println("Периметр круга равен " + p + " см");
     }
 }
