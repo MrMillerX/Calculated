bc39a3f9d9b15985b14adb045b628bdb511d04e0
Miller <1marcusmiller1@gmail.com>
2021-02-12 17:51:46 +0300

    Learn Switch.

--- src/com/learn/Main.java
+++ src/com/learn/Main.java
@@ -5,20 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("введите число 1, 2 или 3: ");
-        double number = scan.nextDouble();
-        if (number==1){
-            System.out.println("Вы ввели число 1. Поздравляю.");
-        }
-        else if (number==2){
-            System.out.println("Вы ввели число 2. Поздравляю.");
-        }
-        else if (number==3){
-            System.out.println("Вы ввели число 3. Поздравляю.");
-        }
-        else{
-            System.out.println("К сожалению, вы не поняли сути данного запроса. Соболезную.");
-        }
+        System.out.print("введите число 1, 2, 3 или 4: ");
+        int number = scan.nextInt();
+       switch (number) {
+           case 1: System.out.println("Вы ввели число 1."); break;
+           case 2: System.out.println("Вы ввели число 2."); break;
+           case 3: System.out.println("Вы ввели число 3."); break;
+           case 4: System.out.println("Вы ввели число 4."); break;
+           default: System.out.println("Вы ввели неверное число.");
+       }
 
     }
 }
