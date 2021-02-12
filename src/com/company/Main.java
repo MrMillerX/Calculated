5568c3cf37ffd0f6561691ae560aad6f8eaceaeb
Miller <1marcusmiller1@gmail.com>
2021-02-12 11:17:25 +0300

    Start

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Напишите всё, что хотите: ");
+        String phrase = scan.nextLine();
+        System.out.println("Вы сказали: "+phrase);
+    }
+}
