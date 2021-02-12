b325902a8e2579e170ad68428b4134a87b3deba8
Miller <1marcusmiller1@gmail.com>
2021-02-12 17:24:28 +0300

    Learn If.

--- src/com/learn/Main.java
+++ src/com/learn/Main.java
@@ -5,11 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        final double PI = 3.1415;
-        double p;
-        System.out.print("Введите значение Радиуса круга:\nR = ");
-        int r = scan.nextInt();
-        p = 2 * PI * r;
-        System.out.println("Периметр круга равен " + p + " см");
+        System.out.print("введите число 1, 2 или 3: ");
+        double number = scan.nextDouble();
+        if (number==1){
+            System.out.println("Вы ввели число 1. Поздравляю.");
+        }
+        else if (number==2){
+            System.out.println("Вы ввели число 2. Поздравляю.");
+        }
+        else if (number==3){
+            System.out.println("Вы ввели число 3. Поздравляю.");
+        }
+        else{
+            System.out.println("К сожалению, вы не поняли сути данного запроса. Соболезную.");
+        }
+
     }
 }
