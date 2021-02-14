860f8ef6e6f9a06779a8f7bd2ae362c26d3d2d26
Miller <1marcusmiller1@gmail.com>
2021-02-15 00:18:05 +0300

    Restructuring GetValue.

--- src/Calculator/Classes/GetSplit.java
+++ src/Calculator/Classes/GetSplit.java
@@ -2,15 +2,24 @@ package Calculator.Classes;
 
 public class GetSplit {
     GetValue getValue = new GetValue();
+
     public int[] splitNums(){
-        String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.)\\s*");
-        int num1 = Integer.parseInt(splitResult[0]);
-        int num2 = Integer.parseInt(splitResult[2]);
-        return new int[]{num1, num2};
+        String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.|\\+|-|/|\\*)\\s*");
+            int[] nums = new int[2];
+            nums[0] = Integer.parseInt(splitResult[0]);
+            nums[1] = Integer.parseInt(splitResult[1]);
+            return nums;
+    }
+    public String[] splitRomanNums(){
+        String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.|\\+|-|/|\\*)\\s*");
+        String[] romanNums = new String[5];
+        romanNums[0] = splitResult[0];
+        romanNums[1] = splitResult[1];
+        return romanNums;
     }
     public String splitSign(){
         String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.)\\s*");
-        String Sign = splitResult[1];
-        return Sign;
+        String sign = splitResult[1];
+        return sign;
     }
 }
