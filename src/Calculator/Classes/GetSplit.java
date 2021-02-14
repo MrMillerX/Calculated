6d5b2aa34c6fc241675604769174fbfb89b12da5
Miller <1marcusmiller1@gmail.com>
2021-02-15 00:36:32 +0300

    Restructuring GetSplit.

--- src/Calculator/Classes/GetSplit.java
+++ src/Calculator/Classes/GetSplit.java
@@ -1,24 +1,26 @@
 package Calculator.Classes;
 
 public class GetSplit {
-    GetValue getValue = new GetValue();
-
+    public String splitValue;
+    public GetSplit(String value) {
+        splitValue = value;
+    }
     public int[] splitNums(){
-        String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.|\\+|-|/|\\*)\\s*");
+        String[] splitResult = splitValue.split("\\s*(\\s|,|!|\\.|\\+|-|/|\\*)\\s*");
             int[] nums = new int[2];
             nums[0] = Integer.parseInt(splitResult[0]);
             nums[1] = Integer.parseInt(splitResult[1]);
             return nums;
     }
     public String[] splitRomanNums(){
-        String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.|\\+|-|/|\\*)\\s*");
+        String[] splitResult = splitValue.split("\\s*(\\s|,|!|\\.|\\+|-|/|\\*)\\s*");
         String[] romanNums = new String[5];
         romanNums[0] = splitResult[0];
         romanNums[1] = splitResult[1];
         return romanNums;
     }
     public String splitSign(){
-        String[] splitResult = getValue.getNumbers().split("\\s*(\\s|,|!|\\.)\\s*");
+        String[] splitResult = splitValue.split("\\s*(\\s|,|!|\\.)\\s*");
         String sign = splitResult[1];
         return sign;
     }
