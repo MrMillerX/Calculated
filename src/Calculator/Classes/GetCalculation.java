2665edd36152d435ec43e8b79a7bcbd62a46b10b
Miller <1marcusmiller1@gmail.com>
2021-02-15 14:48:58 +0300

    Create GetCalculation.

---
+++ src/Calculator/Classes/GetCalculation.java
@@ -0,0 +1,35 @@
+package Calculator.Classes;
+
+public class GetCalculation {
+    int num1;
+    int num2;
+    String sign;
+    double resultDouble;
+    int resultInt;
+
+    GetCalculation(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int addition() {
+
+        resultInt = num1 + num2;
+        return resultInt;
+    }
+
+    public int subtraction() {
+        resultInt = num1 - num2;
+        return resultInt;
+    }
+
+    public int multiplication() {
+        resultInt = num1 * num2;
+        return resultInt;
+    }
+
+    public double division() {
+        resultDouble = num1 / num2;
+        return resultDouble;
+    }
+}
